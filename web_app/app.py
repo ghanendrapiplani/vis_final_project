ef2ca587bf9fc1004c6716c91f1da0eee2be80d7
ghanendrapiplani <ghanendrapiplani@gmail.com>
2020-05-18 19:48:44 -0400

    heat map hover and selection added

--- web_app/app.py
+++ web_app/app.py
@@ -6,40 +6,70 @@ import pandas as pd
 
 app = Flask(__name__)
 
+map_data = {
+   "took": 492,
+   "timed_out": "false",
+   "_shards": {
+      "total": 5,
+      "successful": 3,
+      "failed": 0
+   },
+   "hits": {
+      "total": 30111166,
+      "max_score": 0,
+      "hits": []
+   },
+   "aggregations": {
+      "world_map": {
+         "doc_count_error_upper_bound": 0,
+         "sum_other_doc_count": 0
+      }
+   }
+}
 
 @app.route("/")
 def index():
     global c_dict
     global df_main
-    q1 = """SELECT * FROM df_main where Year = 2015 """
     df = df_main.query("Year == 2015")
-    df['Id'] = ""
-    df['Id'] = df.apply(lambda x: c_dict[x['Country']] if x['Country'] in c_dict.keys() else "", axis=1)
-    print(df[['Country', 'Life expectancy ', 'Id']])
-    df[['Country', 'Life expectancy ', 'Id']].to_csv("test.csv")
-    return render_template("index.html")
+    df_req = df[['Life expectancy ','iso3']]
+    df_req.columns = ['doc_count', 'key']
+    map_data['aggregations']['world_map']['buckets'] = df_req.to_dict('records')
+    print(map_data)
+    return render_template("index.html", data=map_data,
+                           worldJSON=json.load(open("static/world.json")))
+    # return render_template("index.html", data=,)
 
 
 @app.route("/countryselected")
-def kmeans():
+def countryselected():
     # global df_main
     arr = request.args.get('data')
     print(arr.split(','))
-    return render_template("index.html", data="asdf",  title=json.dumps({"title": "PCA for KMeans Clustered Data"}))
+    return render_template("index.html", data="",  title=json.dumps({"title": "PCA for KMeans Clustered Data"}))
 
 
-def fx(row):
-    print("row")
-    print(row)
-    print(row['Country'])
-    print(c_dict[row['Country']])
-    row['Id'] = c_dict[row['Country']]
+def prepare_for_client(df):
+    chart_data = df.to_dict(orient='records')
+    chart_data = json.dumps(chart_data, indent=2)
+    data = {'chart_data': chart_data}
+    return data
+
+
+def fx(df_country_codes, country):
+    val = ""
+    try:
+        val = df_country_codes.query("Combined_Key == '{}'".format(country))['iso3'][0]
+    except Exception as e:
+        print(e)
+    return val
 
 
 if __name__ == "__main__":
     df_main = pd.read_csv('Life_Expectancy_Data.csv', encoding='utf8')
-    print("df mainn")
-    print(df_main.head())
-    print(c_id_dict)
-
+    print(df_main)
+    df_country_codes = pd.read_csv('country_lookup.csv')
+    print(df_country_codes.head())
+    df_main = df_main.merge(df_country_codes, on = 'Country', how='inner')
+    # print(df_main.to_csv("test.csv"))
     app.run(debug=True)
--- web_app/static/mapselector.js
+++ web_app/static/mapselector.js
@@ -1,82 +1,169 @@
-var countries = ["IN", "CN", "KZ", "US"]
-    am4core.ready(function() {
-    
-    // Themes begin
-    am4core.useTheme(am4themes_animated);
-    // Themes end
-    
-    // Create map instance
-    var chart = am4core.create("chartdiv", am4maps.MapChart);
-    
-    // Set map definition
-    chart.geodata = am4geodata_worldLow;
-    
-    // Set projection
-    chart.projection = new am4maps.projections.NaturalEarth1();
-    
-    // Create map polygon series
-    var polygonSeries = chart.series.push(new am4maps.MapPolygonSeries());
-    polygonSeries.mapPolygons.template.strokeWidth = 0.5;
-    
-    // Exclude Antartica
-    polygonSeries.exclude = ["AQ"];
-    
-    // Make map load polygon (like country names) data from GeoJSON
-    polygonSeries.useGeodata = true;
-    
-    // Configure series
-    var polygonTemplate = polygonSeries.mapPolygons.template;
-    polygonTemplate.tooltipText = "{name}";
-    polygonTemplate.fill = chart.colors.getIndex(0);
-    
-    // Create hover state and set alternative fill color
-    var hs = polygonTemplate.states.create("hover");
-    hs.properties.fill = chart.colors.getIndex(2);
-    
-    // Create active state
-    var activeState = polygonTemplate.states.create("active");
-    activeState.properties.fill = chart.colors.getIndex(4);
-    
-    polygonTemplate.sele
+var c_arr = ["IND", "CHN", "AFG", "USA"]
 
-    // Create an event to toggle "active" state
-    polygonTemplate.events.on("hit", function(ev) {
-      ev.target.isActive = !ev.target.isActive;
-      id = ev.target.dataItem.dataContext.id
-      if(countries.indexOf(id) === -1) {
-        countries.push(id);
-        console.log(id);
-        console.log(countries)
-      }else{
-        countries.splice( countries.indexOf(id), 1 );
-        console.log(countries)
+console.log("mapselec")
+console.log(worldjson_mapdata)
+console.log(data_map)
+
+processWorldD(worldjson_mapdata, data_map)
+
+var borderwidth=1.5;
+var bordercolor='red';
+
+function processWorldD(world, data) {
+  var mapdata = {};
+  var palette = ["#ffffd9","#edf8b1","#c7e9b4","#7fcdbb","#41b6c4","#1d91c0","#225ea8","#253494","#081d58"]
+  var width = 550, height = 500;
+  var minDocCount = 0, quantiles = {};
+  // projection definitions
+  var projection = d3.geo.mercator()
+      .scale((width + 1) / 2 / Math.PI)
+      .translate([width/2, height/2])
+      .precision(.1);
+  var path = d3.geo.path().projection(projection);
+  var graticule = d3.geo.graticule();
+  // SVG related definitions
+  var svg = d3.select("#world_map")
+                  .append("svg")
+                  .attr('padding',50)
+                  .attr("width", width)
+                  .attr("height", height)
+                  .append("g");
+  var filter = svg.append('defs')
+      .append('filter')
+      .attr({'x':0, 'y':0, 'width':1, 'height':1, 'id':'gray-background'});
+  filter.append('feFlood')
+      .attr('flood-color', '#f2f2f2')
+      .attr('result', 'COLOR');
+  filter.append('feMorphology')
+      .attr('operator', 'dilate')
+      .attr('radius', '.9')
+      .attr('in', 'SourceAlpha')
+      .attr('result', 'MORPHED');
+  filter.append('feComposite')
+      .attr('in', 'SourceGraphic')
+      .attr('in2', 'MORPHED')
+      .attr('result', 'COMP1');
+  filter.append('feComposite')
+      .attr('in', 'COMP1')
+      .attr('in2', 'COLOR');
+  svg.append("path")
+      .attr("class", "graticule")
+      .attr("d", path);
+      for(var idx=0; idx < data.aggregations.world_map.buckets.length; idx++) {
+          var cCode = data.aggregations.world_map.buckets[idx].key.toUpperCase();
+          var doc_count = data.aggregations.world_map.buckets[idx].doc_count;
+          for(var wdx=0; wdx < world.objects.subunits.geometries.length; wdx++) {
+              var cName = world.objects.subunits.geometries[wdx].id.toUpperCase();
+              if (cCode === cName) {
+                  world.objects.subunits.geometries[wdx].properties.doc_count = doc_count;
+              }
+          }
       }
-      ajax_fx("http://127.0.0.1:5000/radar");
-      ajax_fx("http://127.0.0.1:5000/parallelcord");
-      ajax_fx("http://127.0.0.1:5000/scatter");
-    })
-    
-    chart.events.on("ready", function(ev) {
-      for(var i = 0; i < countries.length; i++) {
-        var country = polygonSeries.getPolygonById(countries[i]);
-        country.isActive = true;
-        
-    }
-    });
+      var subunits = topojson.feature(world, world.objects.subunits);
+      subunits.features = subunits.features.filter(function(d){ return d.id !== "ATA"; });
+      console.log('subunits',subunits);
+      minDocCount = d3.min(subunits.features, function(d){ return d.properties.doc_count; });
+      console.log('minDocCount',minDocCount);
+      var doc_counts = subunits.features.map(function(d){ return d.properties.doc_count; });
+      doc_counts = doc_counts.filter(function(d){ return d; }).sort(d3.ascending);
+      //console.log('doc_counts',doc_counts);
+      quantiles['0.95'] = d3.quantile(doc_counts, '0.95');
+      var countries = svg.selectAll('path.subunit')
+          .data(subunits.features).enter();
+      countries.insert('path', '.graticule')
+          .attr('class', function(d) { return 'subunit ca'+d.id; })
+          .style('fill', heatColor)
+          .attr('d', path)
+          .on('mouseover',mouseoverLegend).on('mouseout',mouseoutLegend)
+          .on('click', coutryclicked);
+      countries.append('svg:text')
+          .attr('class', function(d){ return 'subunit-label la'+d.id+d.properties.name.replace(/[ \.#']+/g,''); })
+          //.attr('transform', function(d) { return 'translate('+ path.centroid(d) +')'; })
+          .attr('transform', function(d) { return 'translate('+(width-(5*d.properties.name.length))+','+(15)+')'; })
+          .attr('dy', '.35em')
+          .attr('filter', 'url(#gray-background)')
+          .append('svg:tspan')
+          .attr('x', 0)
+          .attr('dy', 5)
+          .text(function(d) { return d.properties.name; })
+          .append('svg:tspan')
+          .attr('x', 0)
+          .attr('dy', 20)
+          .text(function(d) { return d.properties.doc_count ? d.properties.doc_count : ''; });
+
+          function mouseoverLegend(datum, index) { 
+              d3.selectAll('.subunit-label.la'+datum.id+datum.properties.name.replace(/[ \.#']+/g,''))
+                  .style('display', 'inline-block').style('margin-right',60);
+                  d3.selectAll('.subunit.ca'+datum.id)
+                  .style('fill', heatColor(datum));
+                  d3.selectAll('.subunit.ca'+datum.id)
+                      .style("stroke", bordercolor) 
+                      .style("stroke-width", borderwidth);
+          }
+          function mouseoutLegend(datum, index) {
+              d3.selectAll('.subunit-label.la'+datum.id+datum.properties.name.replace(/[ \.#']+/g,''))
+                  .style('display', 'none');
+                  if(c_arr.indexOf(datum.id) != -1) {
+                    
+                  }else{
+                    d3.selectAll('.subunit.ca'+datum.id)
+                    .style('fill', heatColor(datum));
+                    d3.selectAll('.subunit.ca'+datum.id)
+                    .style("stroke", "white") 
+                    .style("stroke-width", borderwidth);  
+                  }
+          }
 
 
-    var graticuleSeries = chart.series.push(new am4maps.GraticuleSeries());
-    
-    });
+          function coutryclicked(datum, index) {
+              //filter event for this country should be applied here
 
-    function ajax_fx(url){
-      console.log("ajax for url"+url)
+              id = datum.id
+              if(c_arr.indexOf(id) === -1) {
+                c_arr.push(id);
+                console.log(id);
+               
+                d3.selectAll('.subunit.ca'+datum.id)
+                  .style('fill', heatColor(datum));
+                  d3.selectAll('.subunit.ca'+datum.id)
+                      .style("stroke", bordercolor) 
+                      .style("stroke-width", borderwidth);
+              }else{
+                c_arr.splice( c_arr.indexOf(id), 1 );
+                d3.selectAll('.subunit.ca'+datum.id)
+                  .style('fill', heatColor(datum));
+                d3.selectAll('.subunit.ca'+datum.id)
+                .style("stroke", "white") 
+                .style("stroke-width", borderwidth);
+              }
+              console.log("CARR="+c_arr)
+              // ajax_fx("http://127.0.0.1:5000/radar");
+              // ajax_fx("http://127.0.0.1:5000/parallelcord");
+              // ajax_fx("http://127.0.0.1:5000/scatter");
 
-//      $(".loader").show();
-//      d3.select("svg").remove();
-      $.ajax({url: url, data : {data:countries.join(", ")}, success: function(result){
-        console.log(url)
-//              $(".loader").hide();
-//              $("#div1").html(result);
-      }});
+              
+          }
+          function heatColor(d) {
+              if (quantiles['0.95'] === 0 && minDocCount === 0) return '#F0F0F0';
+              if (!d.properties.doc_count) return '#F0F0F0';
+              if (d.properties.doc_count > quantiles['0.95']) return palette[(palette.length - 1)];
+              if (quantiles['0.95'] == minDocCount) return palette[(palette.length-1)];
+              var diffDocCount = quantiles['0.95'] - minDocCount;
+              var paletteInterval = diffDocCount / palette.length;
+              var diffDocCountDatum = quantiles['0.95'] - d.properties.doc_count;
+              var diffDatumDiffDoc = diffDocCount - diffDocCountDatum;
+              var approxIdx = diffDatumDiffDoc / paletteInterval;
+              if (!approxIdx || Math.floor(approxIdx) === 0) approxIdx = 0;
+              else approxIdx = Math.floor(approxIdx) - 1;
+              return palette[approxIdx];
+          }
+          
+          for(var i = 0; i < c_arr.length; i++) {
+            // coutryclicked(c_arr[i],1)
+            console.log("selected "+c_arr[i])
+                d3.selectAll('.subunit.ca'+c_arr[i])
+                .style("stroke", bordercolor)
+                .style("stroke-width", borderwidth);
+        }
+ 
 }
